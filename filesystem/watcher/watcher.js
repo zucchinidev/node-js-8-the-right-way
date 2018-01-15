03ede1a4797919aa3a8202630b023c3b42e90666
Andrea Zucchini <azga84@gmail.com>
2018-01-15 12:27:33 +0100

    create a separate process to execute the desired command

--- filesystem/watcher/watcher.js
+++ filesystem/watcher/watcher.js
@@ -4,7 +4,25 @@
 *  A simple program that watch files
 *  for changes and read arguments from the command line
 */
+const minimist = require('minimist')
+const spawn = require('child_process').spawn
 
 const fs = require('fs')
-fs.watch('target.txt', () => console.log('File changed!'))
-console.log('Now watching target.txt for changes...')
+const args = minimist(process.argv)
+if (!args.filename) {
+  throw Error('A file to watch must be specified!')
+}
+const { filename } = args
+fs.watch(filename, () => {
+  const ls = spawn('ls', ['-l', '-h', filename])
+  let output = ''
+  ls.stdout.on('data', chunk => (output += chunk))
+  ls.on('close', () => {
+    const parts = output.split(/\s+/)
+    const permissions = parts[0]
+    const size = parts[4]
+    const filename = parts[8]
+    console.log([permissions, size, filename])
+  })
+})
+console.log(`Now watching ${filename} for changes...`)
