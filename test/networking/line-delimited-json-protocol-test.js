8818f7bb6c0105e9e3cd6d9bddc4a6186ec359aa
Andrea Zucchini <azga84@gmail.com>
2018-01-15 18:17:39 +0100

    test: emit a message event from a split data events

---
+++ test/networking/line-delimited-json-protocol-test.js
@@ -0,0 +1,32 @@
+'use strict'
+/* global beforeEach, describe, it */
+const assert = require('assert')
+const { EventEmitter } = require('events')
+const LDJClient = require('../../networking/net-watcher/lib/line-delimited-json-protocol-client')
+describe('LDJClient', () => {
+  let stream = null
+  let client = null
+  beforeEach(() => {
+    stream = new EventEmitter()
+    client = new LDJClient(stream)
+  })
+
+  it('should emit a message event from a single data event', done => {
+    client.on('message', message => {
+      assert.deepEqual(message, { foo: 'bar' })
+      done()
+    })
+
+    stream.emit('data', JSON.stringify({ foo: 'bar' }) + '\n')
+  })
+
+  it('should emit a message event from a split data events', done => {
+    client.on('message', message => {
+      assert.deepEqual(message, { foo: 'bar' })
+      done()
+    })
+
+    stream.emit('data', '{ "foo":')
+    process.nextTick(() => stream.emit('data', '"bar"}\n'))
+  })
+})
