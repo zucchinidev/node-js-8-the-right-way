29eab7b059e7c9057f65eaf5ae94c0f08993dade
Andrea Zucchini <azga84@gmail.com>
2018-01-29 22:15:48 +0100

    listing elasticsearch indices

--- elastic-search-command-line/index.js
+++ elastic-search-command-line/index.js
@@ -7,7 +7,8 @@ const { version, description } = require('./package')
 const COMMANDS = {
   url: 'url [path]',
   get: 'get [path]',
-  createIndex: 'create-index'
+  createIndex: 'create-index',
+  listIndices: 'list-indices'
 }
 
 const fullUrl = (path = '') => {
@@ -21,8 +22,9 @@ const fullUrl = (path = '') => {
   return `${url}${path.replace(/^\/*/, '')}`
 }
 
-const onCreateIndexCommand = (...args) => handleResponse(...args)
-const onGetDataCommand = (...args) => handleResponse(...args)
+const onCreateIndexRequest = (...args) => handleResponse(...args)
+const onGetDataRequest = (...args) => handleResponse(...args)
+const onListIndicesRequest = (...args) => handleResponse(...args)
 
 const handleResponse = (err, res, body) => {
   if (err) {
@@ -34,6 +36,32 @@ const handleResponse = (err, res, body) => {
     return console.log(JSON.stringify(err || body))
   }
   console.log(body)
+}
+
+const onGetDataCommand = (path = '/') => {
+  const options = {
+    url: fullUrl(path),
+    json: commander.json
+  }
+  request(options, onGetDataRequest)
+}
+
+const onCreateIndexCommand = () => {
+  if (!commander.index) {
+    const message = 'No index specified! Use --index <name>'
+    if (!commander.json) {
+      throw new Error(message)
+    }
+    console.log(JSON.stringify({ error: message }))
+    return
+  }
+  request.put(fullUrl(), onCreateIndexRequest)
+}
+
+const onListIndicesCommand = () => {
+  const isJsonFormatSelected = commander.json
+  const path = isJsonFormatSelected ? '_all' : '_cat/indices?v'
+  request({ url: fullUrl(path), json: isJsonFormatSelected }, onListIndicesRequest)
 }
 
 commander
@@ -54,27 +82,16 @@ commander
 commander
   .command(COMMANDS.get)
   .description('perform an HTTP GET request for path (default is /)')
-  .action((path = '/') => {
-    const options = {
-      url: fullUrl(path),
-      json: commander.json
-    }
-    request(options, onGetDataCommand)
-  })
+  .action(onGetDataCommand)
 
 commander.command(COMMANDS.createIndex)
   .description('create an index')
-  .action(() => {
-    if (!commander.index) {
-      const message = 'No index specified! Use --index <name>'
-      if (!commander.json) {
-        throw new Error(message)
-      }
-      console.log(JSON.stringify({ error: message }))
-      return
-    }
-    request.put(fullUrl(), onCreateIndexCommand)
-  })
+  .action(onCreateIndexCommand)
+
+commander.command(COMMANDS.listIndices)
+  .alias('li')
+  .description('get a list of indices in the elasticsearch cluster')
+  .action(onListIndicesCommand)
 
 commander.parse(process.argv)
 if (!commander.args.filter(arg => typeof arg === 'object').length) {
