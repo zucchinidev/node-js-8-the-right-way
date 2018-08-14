c52642ac8083024be9122ad497012e467d974eab
Andrea Zucchini García <azga84@gmail.com>
2018-08-14 21:32:51 +0200

    clean repository

--- first-attempt/filesystem/read-stream/read-stream.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const filename = process.argv[2]
-require('fs').createReadStream(filename)
-  .on('data', chunk => process.stdout.write(chunk))
-  .on('error', err => process.stderr.write(`ERROR -> ${err.message}\n`))
