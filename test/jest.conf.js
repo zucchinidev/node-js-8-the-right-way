cc27bea68bc8c2e395f1ff6ef77888813fbc79d4
Andrea Zucchini García <azga84@gmail.com>
2018-08-15 14:26:29 +0200

    Configure project to create the unit test with Jest

---
+++ test/jest.conf.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  rootDir: path.resolve(__dirname, '../'),
+  moduleFileExtensions: [
+    'js',
+    'json'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/lib/$1'
+  },
+  testPathIgnorePatterns: [
+    '/node_modules/'
+    // add here external dependencies
+  ],
+  setupFiles: ['<rootDir>/test/setup'],
+  coverageDirectory: '<rootDir>/test/coverage',
+  collectCoverageFrom: [
+    'lib/**/*.{js}',
+    '!**/node_modules/**'
+  ]
+}
---
+++ test/specs/networking/lib/line-delimiter-json-client.spec.js
@@ -0,0 +1,5 @@
+describe('line-delimiter-json-client', () => {
+  it(`should be true`, () => {
+    expect(true).toEqual(true)
+  })
+})
