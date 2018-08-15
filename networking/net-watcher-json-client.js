9b899f3a5b401598b9bc0984c1b4be3e9f1c01ff
Andrea Zucchini García <azga84@gmail.com>
2018-08-15 12:57:03 +0200

    create TCP client to receive data from TCP server

---
+++ networking/net-watcher-json-client.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const net = require('net')
+const client = net.connect({ port: 60300 })
+client.on('data', data => {
+  try {
+    const message = JSON.parse(data);
+    (message.type === 'watching')
+      ? console.log(`Now watching: ${message.file}`)
+      : (message.type === 'changed')
+      ? console.log(`File changed: ${new Date(message.timestamp)}`)
+      : console.log(`Unreconized message type: ${message.type}`)
+  } catch (err) {
+
+  }
+})
