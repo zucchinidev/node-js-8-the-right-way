b0f60172fb874fe03f618239c7cd71b6918e2680
Andrea Zucchini García <azga84@gmail.com>
2018-08-15 12:35:19 +0200

    use line delimited json protocol to send message

--- neworking/server.js
+++ neworking/server.js
@@ -3,11 +3,15 @@
 const net = require('net')
 const fs = require('fs')
 
+function write (connection, data) {
+  connection.write(`${JSON.stringify(data)}\n`)
+}
+
 const createServerWatcher = filename => {
   return net.createServer(connection => {
     console.log('Subscriber connected')
-    connection.write(`Now is watching ${filename} for changes...\n`)
-    const watcher = fs.watch(filename, () => connection.write(`File changed: ${new Date()}\n`))
+    write(connection, { type: 'watching', file: filename })
+    const watcher = fs.watch(filename, () => write(connection, { type: 'changed', timestamp: Date.now() }))
     connection.on('close', () => {
       console.log('Subscriber disconnected.')
       watcher.close()
