13cb1ae718f62adbc39f064cf841bd53fb1797c8
Andrea Zucchini García <azga84@gmail.com>
2018-08-25 13:37:54 +0200

    Parser RDF finalized and tested

--- src/databases/lib/parse-rdf.js
+++ src/databases/lib/parse-rdf.js
@@ -1,5 +1,18 @@
 'use strict'
+const cheerio = require('cheerio')
 
 module.exports = rdf => {
-  return {}
+  const $ = cheerio.load(rdf)
+  const bookId = +$('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', '')
+  const title = $('dcterms\\:title').text()
+  const authors = $('pgterms\\:agent pgterms\\:name').toArray().map(e => $(e).text())
+  const subjects = $('[rdf\\:resource$="LCSH"]')
+    .parent().find('rdf\\:value').toArray().map(e => $(e).text())
+
+  return {
+    id: bookId,
+    title,
+    authors,
+    subjects
+  }
 }
--- test/specs/databases/parse-rdf.spec.js
+++ test/specs/databases/parse-rdf.spec.js
@@ -8,6 +8,19 @@ const rdf = fs.readFileSync(url)
 
 describe('parseRDF', () => {
   it(`should parse RDF content`, () => {
-    expect(parseRDF(rdf)).toBeInstanceOf(Object)
+    const book = parseRDF(rdf)
+    expect(book).toBeInstanceOf(Object)
+    expect(book).toEqual({
+      id: 132,
+      title: 'The Art of War',
+      authors: [
+        'Giles, Lionel',
+        'Sunzi, active 6th century B.C.'
+      ],
+      subjects: [
+        'War -- Early works to 1800',
+        'Military art and science -- Early works to 1800'
+      ]
+    })
   })
 })
