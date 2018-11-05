f98bbd54e474e6df30820f37c9d9f147d7059531
Andrea Zucchini García <azga84@gmail.com>
2018-11-05 19:03:45 +0100

    Create get command to retrieve elasticsearch information from its API

--- src/elasticsearch-cli/index.js
+++ src/elasticsearch-cli/index.js
@@ -5,6 +5,30 @@ const request = require('request')
 const program = require('commander')
 const pkg = require('./package')
 
+function fullUrl (path = '') {
+  let url = `http://${program.host}:${program.port}/`
+  if (program.index) {
+    url += `${program.index}/`
+    if (program.type) {
+      url += `${program.type}/`
+    }
+  }
+  return url + path.replace(/^\/*/, '')
+}
+
+function handleResponse (err, res, body) {
+  if (program.json) {
+    console.log(JSON.stringify(err || body))
+  } else {
+    if (err) {
+      throw err
+    }
+    console.log(body)
+  }
+}
+
+const isJson = () => program.json
+
 program
   .version(pkg.version)
   .description(pkg.description)
@@ -14,6 +38,16 @@ program
   .option('-j, --json', 'format output as JSON')
   .option('-i, --index <name>', 'which index to use')
   .option('-t, --type <type>', 'default type for bulk operations')
+
+program.command('get [path]')
+  .description('generate the URL for the options and path (default is /)')
+  .action((path = '/') => {
+    const options = {
+      url: fullUrl(path),
+      json: program.json
+    }
+    request(options, handleResponse)
+  })
 
 program.parse(process.argv)
 const isObject = arg => typeof arg === 'object'
